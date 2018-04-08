466baa34268d6a823923dd9f54f26b416b0f8fef
wangjian <sn_baby@qq.com>
2018-04-08 08:46:22 +0800

    新增

---
+++ src/test/java/com/conan/crawler/server/pre/ConanCrawlerServerPreApplicationTests.java
@@ -0,0 +1,16 @@
+package com.conan.crawler.server.pre;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConanCrawlerServerPreApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
