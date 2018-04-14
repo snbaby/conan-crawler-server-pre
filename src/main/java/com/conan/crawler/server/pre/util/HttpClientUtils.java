d4ed4472d769ce72e3f48143820c5b2c869de115
wangjian <wangj@seadun.com>
2018-04-14 17:23:51 +0800

    新增 http 传输

---
+++ src/main/java/com/conan/crawler/server/pre/util/HttpClientUtils.java
@@ -0,0 +1,113 @@
+package com.conan.crawler.server.pre.util;
+
+import java.nio.charset.Charset;
+import java.util.List;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.CoreConnectionPNames;
+
+import net.sf.json.JSONObject;
+
+public class HttpClientUtils {
+	public static boolean httpPostWithForm(List<NameValuePair> nvps,String url){
+	    boolean isSuccess = false;
+	    
+	    HttpPost post = null;
+	    try {
+	        HttpClient httpClient = new DefaultHttpClient();
+
+	        // 设置超时时间
+	        httpClient.getParams().setParameter(CoreConnectionPNames.CONNECTION_TIMEOUT, 2000);
+	        httpClient.getParams().setParameter(CoreConnectionPNames.SO_TIMEOUT, 2000);
+	            
+	        post = new HttpPost(url);
+	        // 构造消息头
+	        post.setHeader("Content-type", "application/x-www-form-urlencoded; charset=utf-8");
+	        post.setHeader("Connection", "Close");
+	                    
+	        // 构建消息实体
+	        post.setEntity(new UrlEncodedFormEntity(nvps, "UTF-8"));
+	            
+	        HttpResponse response = httpClient.execute(post);
+	            
+	        // 检验返回码
+	        int statusCode = response.getStatusLine().getStatusCode();
+	        if(statusCode != HttpStatus.SC_OK){
+	            System.out.println("请求出错: "+url);
+	            isSuccess = false;
+	        }else{
+	        	System.out.println("请求成功: "+url);
+	            isSuccess = false;
+	        }
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	        isSuccess = false;
+	    }finally{
+	        if(post != null){
+	            try {
+	                post.releaseConnection();
+	                Thread.sleep(500);
+	            } catch (InterruptedException e) {
+	                e.printStackTrace();
+	            }
+	        }
+	    }
+	    return isSuccess;
+	}
+	public static boolean httpPostWithJson(JSONObject jsonObj,String url){
+	    boolean isSuccess = false;
+	    
+	    HttpPost post = null;
+	    try {
+	        HttpClient httpClient = new DefaultHttpClient();
+
+	        // 设置超时时间
+	        httpClient.getParams().setParameter(CoreConnectionPNames.CONNECTION_TIMEOUT, 2000);
+	        httpClient.getParams().setParameter(CoreConnectionPNames.SO_TIMEOUT, 2000);
+	            
+	        post = new HttpPost(url);
+	        // 构造消息头
+	        post.setHeader("Content-type", "application/json; charset=utf-8");
+	        post.setHeader("Connection", "Close");
+	                    
+	        // 构建消息实体
+	        StringEntity entity = new StringEntity(jsonObj.toString(), Charset.forName("UTF-8"));
+	        entity.setContentEncoding("UTF-8");
+	        // 发送Json格式的数据请求
+	        entity.setContentType("application/json");
+	        post.setEntity(entity);
+	            
+	        HttpResponse response = httpClient.execute(post);
+	            
+	        // 检验返回码
+	        int statusCode = response.getStatusLine().getStatusCode();
+	        if(statusCode != HttpStatus.SC_OK){
+	            System.out.println("请求出错: "+url);
+	            isSuccess = false;
+	        }else{
+	        	System.out.println("请求成功: "+url);
+	            isSuccess = false;
+	        }
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	        isSuccess = false;
+	    }finally{
+	        if(post != null){
+	            try {
+	                post.releaseConnection();
+	                Thread.sleep(500);
+	            } catch (InterruptedException e) {
+	                e.printStackTrace();
+	            }
+	        }
+	    }
+	    return isSuccess;
+	}
+}
