2d07536811592021aafbfec1058400c4cbc776b6
wangjian <sn_baby@qq.com>
2018-04-16 01:42:16 +0800

    修改

--- src/main/java/com/conan/crawler/server/pre/rest/DataReportController.java
+++ src/main/java/com/conan/crawler/server/pre/rest/DataReportController.java
@@ -10,14 +10,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import com.conan.crawler.server.pre.entity.AnalysisV;
 import com.conan.crawler.server.pre.entity.CommentScanTb;
-import com.conan.crawler.server.pre.entity.CommentTb;
-import com.conan.crawler.server.pre.entity.GoodsTb;
-import com.conan.crawler.server.pre.entity.ShopScanTb;
+import com.conan.crawler.server.pre.mapper.AnalysisVMapper;
 import com.conan.crawler.server.pre.mapper.CommentScanTbMapper;
-import com.conan.crawler.server.pre.mapper.CommentTbMapper;
-import com.conan.crawler.server.pre.mapper.GoodsTbMapper;
-import com.conan.crawler.server.pre.mapper.ShopScanTbMapper;
 import com.conan.crawler.server.pre.util.HttpClientUtils;
 import com.conan.crawler.server.pre.util.Utils;
 
@@ -26,84 +22,89 @@ import net.sf.json.JSONObject;
 
 @Component
 public class DataReportController {
-	@Autowired
-	private CommentTbMapper commentTbMapper;
 
 	@Autowired
 	private CommentScanTbMapper commentScanTbMapper;
 
 	@Autowired
-	private ShopScanTbMapper shopScanTbMapper;
-
-	@Autowired
-	private GoodsTbMapper goodsTbMapper;
+	private AnalysisVMapper analysisVMapper;
 	
 	@Value("${conan.url.analysis}")
 	private String analysisUrl;
 	
-	@Scheduled(fixedDelay = 2000, initialDelay=60000)
+	@Scheduled(fixedDelay = 1000, initialDelay=60000)
 	public void postRateScanStart() {
 		System.out.println("postRateScanStart--"+analysisUrl);
-		List<CommentScanTb> updateDataList = new ArrayList<>();
-		List<CommentScanTb> commentScanTbList = commentScanTbMapper.selectByStatus("0");
-		System.out.println("postRateScanStart--"+commentScanTbList.size());
-		JSONArray jsonArray = new JSONArray();
-		for(CommentScanTb commentScanTb:commentScanTbList) {
-			GoodsTb goodsTb = goodsTbMapper.selectByItemId(commentScanTb.getItemId());
-			if(goodsTb == null) {
-				continue;
-			}
-			CommentTb commentTb = commentTbMapper.selectByItemId(commentScanTb.getItemId());
-			if(commentTb == null) {
-				continue;
-			}
-			ShopScanTb shopScanTb = shopScanTbMapper.selectByUserNumberId(goodsTb.getUserNumberId());
-			if(shopScanTb == null) {
-				continue;
-			}
-			updateDataList.add(commentScanTb);
-			JSONObject jsonObject = new JSONObject();
-			if(goodsTb.getShopType().equals("0")) {
-				jsonObject.put("catalog", "TmallRate");
-			}else {
-				jsonObject.put("catalog", "TaobaoRate");
-			}
-			jsonObject.put("item_id", commentScanTb.getItemId());
-			jsonObject.put("item_title", goodsTb.getItemTitle());
-			jsonObject.put("shop_name", shopScanTb.getShopName());
-			jsonObject.put("seller_nick", shopScanTb.getSellerNick());
-			jsonObject.put("shop_star", shopScanTb.getShopStar());
-			jsonObject.put("dsr_desc", shopScanTb.getDsrDesc());
-			jsonObject.put("dsr_serv", shopScanTb.getDsrServ());
-			jsonObject.put("dsr_logi", shopScanTb.getDsrLogi());
-			jsonObject.put("total_review_cnt", commentTb.getTotal());
-			jsonObject.put("buyer_nick", commentScanTb.getBuyerNick());
-			jsonObject.put("first_review_time", Utils.getDateString(commentScanTb.getFirstReviewTime()));
-			jsonObject.put("review_content", commentScanTb.getReviewContent());
-			jsonObject.put("is_super_vip", commentScanTb.getIsSuperVip());
-			jsonObject.put("buyer_star", commentScanTb.getBuyerStar());
-			jsonObject.put("gmt_create_time", Utils.getDateString(commentScanTb.getCrtTime()));
-			jsonObject.put("crawler_machine_id", commentScanTb.getCrtIp());
-			jsonObject.put("shop_id", shopScanTb.getShopId());
-			jsonArray.add(jsonObject);
-			
+		List<AnalysisV> analysisVList = analysisVMapper.selectAll();
+		System.out.println("postRateScanStart--"+analysisVList.size());
+		if(analysisVList == null || analysisVList.isEmpty()) {
+			System.out.println("postRateScanStart--null--"+analysisVList.size());
+			return;
 		}
-		if(updateDataList.size()>0) {
-			List<NameValuePair> nvps = new ArrayList<NameValuePair>();
-			nvps.add(new BasicNameValuePair("version", "1"));
-			nvps.add(new BasicNameValuePair("biztype", "1"));
-			nvps.add(new BasicNameValuePair("jdata", jsonArray.toString()));
-			nvps.add(new BasicNameValuePair("sign", Utils.getSign(jsonArray.toString())));
-			System.out.println("start---data-post---");
-			if(HttpClientUtils.httpPostWithForm(nvps, analysisUrl)) {
-				for(CommentScanTb commentScanTb:updateDataList) {
-					commentScanTb.setStatus("1");
-					commentScanTbMapper.updateByPrimaryKey(commentScanTb);
-				}
-				System.out.println("end---data-post---");
-			}else {
-				System.out.println("exception---data-post---");
+		JSONArray jsonArray = new JSONArray();
+		for(AnalysisV analysisV:analysisVList) {
+			JSONObject jsonObject = new JSONObject();
+			jsonObject.put("catalog", analysisV.getCatalog());
+			jsonObject.put("item_id", analysisV.getItemId());
+			jsonObject.put("item_title", analysisV.getItemTitle());
+			jsonObject.put("shop_name", analysisV.getShopName());
+			jsonObject.put("seller_nick", analysisV.getSellerNick());
+			jsonObject.put("shop_star", analysisV.getShopStar());
+			jsonObject.put("dsr_desc", analysisV.getDsrDesc());
+			jsonObject.put("dsr_serv", analysisV.getDsrServ());
+			jsonObject.put("dsr_logi", analysisV.getDsrLogi());
+			jsonObject.put("total_review_cnt", analysisV.getTotal());
+			jsonObject.put("buyer_nick", analysisV.getBuyerNick());
+			jsonObject.put("first_review_time", Utils.getDateString(analysisV.getFirstReviewTime()));
+			jsonObject.put("review_content", analysisV.getReviewContent());
+			jsonObject.put("is_super_vip", analysisV.getIsSuperVip());
+			jsonObject.put("buyer_star", analysisV.getBuyerStar());
+			jsonObject.put("gmt_create_time", Utils.getDateString(analysisV.getCrtTime()));
+			jsonObject.put("crawler_machine_id", analysisV.getCrtIp());
+			jsonObject.put("shop_id", analysisV.getShopId());
+			jsonArray.add(jsonObject);
+		}
+		List<NameValuePair> nvps = new ArrayList<NameValuePair>();
+		nvps.add(new BasicNameValuePair("version", "1"));
+		nvps.add(new BasicNameValuePair("biztype", "1"));
+		nvps.add(new BasicNameValuePair("jdata", jsonArray.toString()));
+		nvps.add(new BasicNameValuePair("sign", Utils.getSign(jsonArray.toString())));
+		System.out.println("start---data-post---");
+		if(HttpClientUtils.httpPostWithForm(nvps, analysisUrl)) {
+			for(AnalysisV analysisV:analysisVList) {
+				CommentScanTb commentScanTb = commentScanTbMapper.selectByPrimaryKey(analysisV.getId());
+				commentScanTb.setStatus("1");
+				commentScanTbMapper.updateByPrimaryKey(commentScanTb);
 			}
+			System.out.println("end---data-post---");
+		}else {
+			System.out.println("exception---data-post---");
 		}
 	}
+	
+	/*
+	 * public static void main(String[] args) { JSONArray jsonArray = new
+	 * JSONArray(); JSONObject jsonObject = new JSONObject();
+	 * jsonObject.put("catalog", "TmallRate"); jsonObject.put("item_id",
+	 * "558485793759"); jsonObject.put("item_title",
+	 * "朴西 家居情侣全包跟棉拖鞋男室内居家防滑厚底保暖月子拖鞋女冬"); jsonObject.put("shop_name", "朴西旗舰店");
+	 * jsonObject.put("seller_nick", "朴西旗舰店"); jsonObject.put("shop_star", "");
+	 * jsonObject.put("dsr_desc", "4.9"); jsonObject.put("dsr_serv", "4.8");
+	 * jsonObject.put("dsr_logi", "4.8"); jsonObject.put("total_review_cnt", 17243);
+	 * jsonObject.put("buyer_nick", "t***i"); jsonObject.put("first_review_time",
+	 * Utils.getDateString("1517139617000")); jsonObject.put("review_content", "");
+	 * jsonObject.put("is_super_vip", true); jsonObject.put("buyer_star", "");
+	 * jsonObject.put("gmt_create_time", "2018-04-16 00:34:16");
+	 * jsonObject.put("crawler_machine_id", "172.27.16.6");
+	 * jsonObject.put("shop_id", "'109604918'"); jsonArray.add(jsonObject);
+	 * 
+	 * List<NameValuePair> nvps = new ArrayList<NameValuePair>(); nvps.add(new
+	 * BasicNameValuePair("version", "1")); nvps.add(new
+	 * BasicNameValuePair("biztype", "1")); nvps.add(new BasicNameValuePair("jdata",
+	 * jsonArray.toString())); nvps.add(new BasicNameValuePair("sign",
+	 * Utils.getSign(jsonArray.toString())));
+	 * 
+	 * HttpClientUtils.httpPostWithForm(nvps,
+	 * "http://223.93.147.217:4433/PtBrain/listen"); }
+	 */
 }
